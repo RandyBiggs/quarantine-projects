643b4c8d9843ef0561744714a976921d284f5580
Randy <randybiggs.spsd@gmail.com>
2020-04-13 02:11:57 -0700

    Add comments and fix formatting for day 4

--- Day 4 - Trivial Array Manipulation/main.py
+++ Day 4 - Trivial Array Manipulation/main.py
@@ -1,14 +1,18 @@
 # Given an input of a list and an index, return the same list without the specified index
 # AKA Didn't have an idea for today
 
+
 def remove_element(lst, idx):
     ret_lst = []
 
+    # Index is beyond the given list
     if idx > len(lst):
         print("Index Out of Bounds")
         ret_lst = -1
+    # Index is negative, i.e. end of list indexing
     elif idx < 0:
         ret_lst = lst[0:idx]
+    # Expected use-case
     else:
         for i in range(len(lst)):
             if i == idx:
@@ -20,4 +24,4 @@ def remove_element(lst, idx):
 
 
 # Test
-remove_element([1,2,3,4,5], 0)
+remove_element([1, 2, 3, 4, 5], 0)
